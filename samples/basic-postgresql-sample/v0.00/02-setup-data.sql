aa67fdd0fb1668a19084e25c31b8b2327cd6a889
srinivas-byte <55057599+srinivas-byte@users.noreply.github.com>
2021-02-24 16:48:07 +0530

    Update 02-setup-data.sql

--- samples/basic-postgresql-sample/v0.00/02-setup-data.sql
+++ samples/basic-postgresql-sample/v0.00/02-setup-data.sql
@@ -4,6 +4,7 @@ INSERT INTO regions(region_id,region_name) VALUES (1,'Europe');
 INSERT INTO regions(region_id,region_name) VALUES (2,'Americas');
 INSERT INTO regions(region_id,region_name) VALUES (3,'Asia');
 INSERT INTO regions(region_id,region_name) VALUES (4,'Middle East and Africa');
+INSERT INTO regions(region_id,region_name) VALUES (6,'aus');
  
  
 /*Data for the table countries */
